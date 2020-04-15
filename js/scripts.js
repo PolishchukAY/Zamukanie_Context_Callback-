d279913a631f5ec9648a85fecaac35d97e9948e5
Polishchukay <polishchukay@gmail.com>
2020-04-15 09:46:42 +0300

    "this" and context "this"

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+	
+	var item = document.querySelector('.items .item');
+	
+	item.onclick = activeItem;
+	{
+		//alert(1);
+	}
+	
+	activeItem();
+}
+
+function activeItem(){
+	//console.log(this)
+	this
+	
+}
