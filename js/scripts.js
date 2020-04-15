e96f389f874884731a93361544640dd252510a9a
Polishchukay <polishchukay@gmail.com>
2020-04-15 10:47:12 +0300

    zamikanie: function outside another function 'not defind' git add --all!

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,17 @@ window.onload = function(){
 	
 	var item = document.querySelector('.items .item');
 	
-	//item.onclick = activeItem;
+	item.onclick = activeItem;
+	
+	/* 
 	item.onclick = function(){
 		this.classList.toggle('item-active');
-	} 
+	} */ 
 }
 
 function activeItem(){
 	//console.log(this)
 	//this
 	this.classList.toggle('item-active');
+	console.log(item);
 }
